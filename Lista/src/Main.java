332df0c8a91e3ac81986874b7eb7befb447c43b0
Pedro Arthur <pedroarthursena95@gmail.com>
2026-08-13 16:10:22 -0300

    Exercício 01

--- Lista/src/Main.java
+++ Lista/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class Main{
     static void main() {
-
+        Scanner entrada = new Scanner(System.in);
+        int a = entrada.nextInt();
+        int mult = 1;
+        for (int i = 1; a >= i; i++) {
+            mult = mult * i;
+        }
+        System.out.println(mult);
     }
 }
