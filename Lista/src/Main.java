2e3008705273221431cdbabfb01e33a78aef519f
Pedro Arthur <pedroarthursena95@gmail.com>
2026-08-13 17:06:04 -0300

    Exercício 2

--- Lista/src/Main.java
+++ Lista/src/Main.java
@@ -2,11 +2,36 @@ import java.util.Scanner;
 public class Main{
     static void main() {
         Scanner entrada = new Scanner(System.in);
-        int a = entrada.nextInt();
-        int mult = 1;
-        for (int i = 1; a >= i; i++) {
-            mult = mult * i;
+        int[] vetor = {entrada.nextInt(), entrada.nextInt(), entrada.nextInt()};
+        int maior = vetor[0];
+        int menor = vetor[0];
+        int x = vetor[0];
+        int y = vetor[1];
+        int z = vetor[2];
+        for (int i = 0; i < vetor.length; i++) {
+            if(vetor[i] > maior){
+                maior = vetor[i];
+            }if(vetor[i] < menor){
+                menor = vetor[i];
+            }
         }
-        System.out.println(mult);
+        System.out.println("O maior número é: "+maior);
+        System.out.println("O menor número é: "+menor);
+        if ((x >= y && x <= z) || (x >= z && x <= y)) {
+            System.out.println("O número X (" + x + ") ESTÁ DENTRO do intervalo");
+        } else {
+            System.out.println("O número X (" + x + ") ESTÁ FORA do intervalo");
+        }
+        if (y != 0 && x % y == 0) {
+            System.out.println("X (" + x + ") é divisível por Y (" + y + ")");
+        } else {
+            System.out.println("X (" + x + ") NÃO é divisível por Y (" + y + ")");
+        }
+        if (z != 0 && x % z == 0) {
+            System.out.println("X (" + x + ") é divisível por Z (" + z + ")");
+        } else {
+            System.out.println("X (" + x + ") NÃO é divisível por Z (" + z + ")");
+        }
+
     }
 }
