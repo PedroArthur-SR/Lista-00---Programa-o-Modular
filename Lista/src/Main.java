fb4b5eac2abf16ad2faef689691217b412baaba9
Pedro Arthur <pedroarthursena95@gmail.com>
2026-08-14 16:16:15 -0300

    Exercício 3

--- Lista/src/Main.java
+++ Lista/src/Main.java
@@ -1,12 +1,24 @@
 import java.util.Scanner;
 public class Main{
     static void main() {
-        Scanner entrada = new Scanner(System.in);
-        int a = entrada.nextInt();
-        int mult = 1;
-        for (int i = 1; a >= i; i++) {
-            mult = mult * i;
+        Scanner entradaMatricula = new Scanner(System.in);
+        int numMatricula = entradaMatricula.nextInt();
+        int [] programacao = new int[numMatricula];
+        int [] calculo = new int[numMatricula];
+        for (int i = 0; i < numMatricula; i++) {
+            System.out.println("Matrícula dos alunos cursando Programação Modular: ");
+            programacao[i] = entradaMatricula.nextInt();
         }
-        System.out.println(mult);
+        for (int i = 0; i < numMatricula; i++) {
+            System.out.println("Matrícula dos alunos cursando cálculo :");
+            calculo[i] = entradaMatricula.nextInt();
+        }
+        for (int i = 0; i < numMatricula; i++) {
+            for (int j = 0; j < numMatricula; j++) {
+                if(calculo[j] == programacao[i]){
+                    System.out.println("Alunos que fazem as duas matérias: "+calculo[j]);
+                }
+            }
+        }
     }
 }
