350d1490334d48ecae599dba84f4dc3000a50516
Pedro Arthur <pedroarthursena95@gmail.com>
2026-08-14 17:50:43 -0300

    Exercício 6

--- Lista/src/Main.java
+++ Lista/src/Main.java
@@ -1,24 +1,31 @@
 import java.util.Scanner;
 public class Main{
     static void main() {
-        Scanner entradaMatricula = new Scanner(System.in);
-        int numMatricula = entradaMatricula.nextInt();
-        int [] programacao = new int[numMatricula];
-        int [] calculo = new int[numMatricula];
-        for (int i = 0; i < numMatricula; i++) {
-            System.out.println("Matrícula dos alunos cursando Programação Modular: ");
-            programacao[i] = entradaMatricula.nextInt();
+        Scanner entrada = new Scanner(System.in);
+        char[] gabarito = new char[8];
+        int aprovados = 0;
+        for (int i = 0; i < 8; i++) {
+            gabarito[i] = entrada.next().charAt(0);
         }
-        for (int i = 0; i < numMatricula; i++) {
-            System.out.println("Matrícula dos alunos cursando cálculo :");
-            calculo[i] = entradaMatricula.nextInt();
-        }
-        for (int i = 0; i < numMatricula; i++) {
-            for (int j = 0; j < numMatricula; j++) {
-                if(calculo[j] == programacao[i]){
-                    System.out.println("Alunos que fazem as duas matérias: "+calculo[j]);
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Digite o número do aluno:");
+            int numeroAluno = entrada.nextInt();
+            int nota = 0;
+            System.out.println("Digite as 8 respostas do aluno:");
+            for (int j = 0; j < 8; j++) {
+                char resposta = entrada.next().charAt(0);
+                if (resposta == gabarito[j]) {
+                    nota++;
                 }
             }
+
+            System.out.println("Aluno " + numeroAluno + " - Nota: " + nota);
+
+            if (nota >= 6) {
+                aprovados++;
+            }
         }
+        double porcentagem = (aprovados / 10.0) * 100;
+        System.out.println("Porcentagem de aprovação: " + porcentagem + "%");
     }
 }
