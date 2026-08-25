8634e7638636385c0d377ab3b7052c754f71dd43
Pedro Arthur <pedroarthursena95@gmail.com>
2026-08-25 14:30:03 -0300

    Exercício 8

--- Lista/src/Main.java
+++ Lista/src/Main.java
@@ -2,32 +2,36 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner entrada = new Scanner(System.in);
-        String[] meses = {
-                "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
-                "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
-        };
-
-        double[] temperaturas = new double[12];
-        for (int i = 0; i < 12; i++) {
-            System.out.println("Digite a temperatura média de " + meses[i] + ":");
-            temperaturas[i] = entrada.nextDouble();
+        double[] valorPrecos = new double[10];
+        int[] qtdPecas = new int[10];
+        for (int i = 0; i < valorPrecos.length; i++) {
+            System.out.println("Informe o preço do objeto: ");
+            valorPrecos[i] = entrada.nextDouble();
+            System.out.println("Informe a quantidade de objetos: ");
+            qtdPecas[i] = entrada.nextInt();
         }
-        double maiorTemp = temperaturas[0];
-        int posMaior = 0;
+        double valorGeralVendas = 0;
+        int maiorQtd = 0;
+        int posicaoMaisVendido = 0;
 
-        double menorTemp = temperaturas[0];
-        int posMenor = 0;
-        for (int i = 1; i < 12; i++) {
-            if (temperaturas[i] > maiorTemp) {
-                maiorTemp = temperaturas[i];
-                posMaior = i;
-            }
-            if (temperaturas[i] < menorTemp) {
-                menorTemp = temperaturas[i];
-                posMenor = i;
+        for (int i = 0; i < 10; i++) {
+            double valorTotalObjeto = valorPrecos[i] * qtdPecas[i];
+            valorGeralVendas += valorTotalObjeto;
+            System.out.println("Objeto "+ (i + 1) +
+            " | Qtd: "+qtdPecas[i] +
+                    " | Valor Unitário: R$ " + valorPrecos[i] +
+                    " | Total: R$ " + valorTotalObjeto);
+            if (qtdPecas[i] > maiorQtd){
+                maiorQtd = qtdPecas[i];
+                posicaoMaisVendido = i;
             }
         }
-        System.out.println("\nMaior temperatura: " + maiorTemp + "°C em " + meses[posMaior]);
-        System.out.println("Menor temperatura: " + menorTemp + "°C em " + meses[posMenor]);
+        double comissao = valorGeralVendas * 0.05;
+
+        System.out.println("----------------------------------");
+        System.out.println("Valor geral das vendas: R$ " + valorGeralVendas);
+        System.out.println("Valor da comissão (5%): R$ " + comissao);
+        System.out.println("Objeto mais vendido: Posição " + posicaoMaisVendido +
+                " (Preço: R$ " + valorPrecos[posicaoMaisVendido] + ")");
     }
 }
