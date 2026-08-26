3c1077a234514ac4188911b5ccb8c72722efac84
Pedro Arthur <pedroarthursena95@gmail.com>
2026-08-26 17:19:30 -0300

    Exercício 10

--- Lista/src/Main.java
+++ Lista/src/Main.java
@@ -2,26 +2,36 @@ import java.util.Scanner;
 public class Main {
     static void main() {
         Scanner entrada = new Scanner(System.in);
-        int[] vetor = new int[6];
-        for (int i = 0; i < vetor.length; i++) {
-            System.out.println("Digite os 6 números: ");
-            vetor[i] = entrada.nextInt();
-        }
-        int somaPares = 0;
-        for (int i = 0; i < vetor.length; i++) {
-            if(vetor[i] % 2 == 0){
-                System.out.println("Número: "+vetor[i]+ " na posição "+(i + 1));
-                somaPares += vetor[i];
+        String[] meses = {
+                "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+                "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+        };
+        double[][] matriz = new double[12][4];
+        double totalAno = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = entrada.nextDouble();
             }
         }
-        System.out.println("A soma dos pares é: "+somaPares);
-        int qtdImpares = 0;
-        for (int i = 0; i < vetor.length; i++) {
-            if(vetor[i] % 2 != 0){
-                System.out.println("Número: "+vetor[i]+ " na posição "+(i + 1));
-                qtdImpares ++;
+        System.out.println("-------- TOTAL POR MÊS ---------");
+        for (int i = 0; i < matriz.length; i++) {
+            double somaMes = 0;
+
+            for (int j = 0; j < matriz[i].length; j++) {
+                somaMes += matriz[i][j];
+                totalAno += matriz[i][j];
             }
+            System.out.println("A soma total do mês "+meses[i]+" foi de R$"+somaMes );
         }
-        System.out.println("Quantidade de ímpares:"+qtdImpares);
+        System.out.println("\n-------- TOTAL POR SEMANA NO ANO ---------");
+        for (int j = 0; j < 4; j++){
+            double totalSemana = 0;
+            for (int i = 0; i < 12; i++) {
+                totalSemana += matriz[i][j];
+            }
+            System.out.println("Total vendido na semana "+(j + 1)+" no ano: R$"+String.format("%.2f", totalSemana));
+        }
+        System.out.println("\n--------TOTAL ANO---------");
+        System.out.println("O total vendido no ano foi de R$"+totalAno);
     }
 }
